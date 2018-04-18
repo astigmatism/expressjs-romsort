f188bd99ef634549ad31debc086f947c5309668b
astigmatism <eric@crazyerics.com>
2018-04-18 15:43:54 -0700

    changed findbest rom to respect translations

--- findbestrom.js
+++ findbestrom.js
@@ -29,10 +29,14 @@ FindBestRom.exec = function(files, officialscore) {
         c:      new RegExp('\\(c\\)', 'ig'),        //Canada release
         a:      new RegExp('\\(a\\)', 'ig'),        //Ausrilia release
         eb:     new RegExp('\\(eb\\)', 'ig'),        //Europe and Brazil
-        e:      new RegExp('\\(e\\)', 'ig'),        //Europe release (last ditch check as can be english sometimes)
-        engn:    new RegExp('\\[t\\+eng(\d|\.).*\\]', 'ig'),
-        eng:    new RegExp('\\[t\\+eng', 'ig')             //English translation of japanese game. I'm putting this up here so that it DOES appear in suggestions and searches
+        e:      new RegExp('\\(e\\)', 'ig')        //Europe release (last ditch check as can be english sometimes)
     };
+
+    var reTrans = {
+        engn:    new RegExp('t\\+eng(\\d+\\.\\d+)', 'ig'),   //t+eng + means newer translation (with version number)
+        engo:    new RegExp('t\\-eng(\\d+\\.\\d+)', 'ig'),   //t-eng - means older translation (with version number)
+        eng:    new RegExp('t[\\+\\-]eng', 'ig')             //t+\-eng no version number
+    }
 
     //regions not in english are still important so that they arent ranked low
     var reRegion2 = {
@@ -60,6 +64,7 @@ FindBestRom.exec = function(files, officialscore) {
         v3:     new RegExp('\\(V1\\.3\\)', 'ig'),   //when a game has a version greater than (V1.0), we want to give it a higher ranking
         v2:     new RegExp('\\(V1\\.2\\)', 'ig'),
         v1:     new RegExp('\\(V1\\.1\\)', 'ig'),
+        v0:     new RegExp('\\(V1\\.0\\)', 'ig'),
         c:      new RegExp('\\[C\\]', 'ig'),        //color!
         p:      new RegExp('\\[!\\]', 'ig'),        //The ROM is an exact copy of the original game; it has not had any hacks or modifications.
         f:      new RegExp('\\[f\d?\\]', 'ig'),        //A fixed dump is a ROM that has been altered to run better on a flashcart or an emulator.
@@ -77,13 +82,14 @@ FindBestRom.exec = function(files, officialscore) {
 
     var result = null;      //the resulting file by name
     var resultindex = 0;    //the index of the resulting game in the "files" array
-    var resultrank = 0;
+    var resultScore = 0;
 
     //pass over all files. as soon as we find a successful match, break out and try the next file
     for (i = 0; i < files.length; ++i) {
 
         var item = files[i];
-        var currentrank = 500;  //starts high, decrements on each pass
+        var fallingScore = 500;  //starts high, decrements on each pass
+        var itemScore = 0;
 
         if (item === '.DS_Store') {
             continue;
@@ -102,138 +108,134 @@ FindBestRom.exec = function(files, officialscore) {
 
         //let's give bonus points (less than 1) at the start of each file before current begins to decrement
         if (item.match(reOption.c)) {
-            currentrank += 0.1;
+            fallingScore += 0.1;
         }
-
+        
         if (item.match(reOption.v3)) {
-            currentrank += 0.3;
+            fallingScore += 0.1;
         }
 
         if (item.match(reOption.v2)) {
-            currentrank += 0.2
+            fallingScore += 0.2
         }
 
         if (item.match(reOption.v1)) {
-            currentrank += 0.1
+            fallingScore += 0.3
+        }
+
+        if (item.match(reOption.v0)) {
+            fallingScore += 0.4;
         }
         //end bonuses
 
 
         //pass over all english regions with playable [!] //450 to 500
         for (re in reRegion) {
-            if (item.match(reRegion[re]) && item.match(reOption.p) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion[re]) && item.match(reOption.p) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
-        currentrank = 450;
+        fallingScore = 450;
 
         //pass over all english regions, no brackets 400 - 450
         for (re in reRegion) {
-            if (item.match(reRegion[re]) && !item.match(reOption.b) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion[re]) && !item.match(reOption.b) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
-        //pass over all english regions brackets ok
-        for (re in reRegion) {
-            if (item.match(reRegion[re]) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+        //consider english translations, cant have more than the brackets surrounding the [t+eng] statement
+        for (re in reTrans) {
+            var brackets = item.match(reOption.b);
+            if (item.match(reTrans[re]) && (brackets.length == 1) && itemScore < fallingScore) {
+                itemScore = fallingScore;
+
+                //parse out the version number of the translation and add it to the score (higher vesions will edge out)
+                var groups = reTrans[re].exec(item);
+                if (groups[1]) {
+                    itemScore += parseFloat(groups[1]);
+                }
             }
-            --currentrank;
+            --fallingScore;
         }
 
-        currentrank = 400; //line between domestic and foreign!!
+        fallingScore = 400; //line between domestic and foreign!! (> 400)
 
         //all non-english regions with playable [!] above 350
         for (re in reRegion2) {
-            if (item.match(reRegion2[re]) && item.match(reOption.p) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion2[re]) && item.match(reOption.p) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
-        currentrank = 350;
+        fallingScore = 350;
 
         //pass over all non-english regions, no brackets above 300
         for (re in reRegion2) {
-            if (item.match(reRegion2[re]) && !item.match(reOption.b) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion2[re]) && !item.match(reOption.b) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
-        currentrank = 300;
+        fallingScore = 300;
 
         //has playable [!] with no matching region data above 250
-        if (item.match(reOption.p) && resultrank < currentrank) {
-            result = item;
-            resultindex = i;
-            resultrank = currentrank;
+        if (item.match(reOption.p) && itemScore < fallingScore) {
+            itemScore = fallingScore;
         }
-        --currentrank;
+        --fallingScore;
 
-        currentrank = 250; //cut off box front art here, chances that anything lower won't have art is high
+        fallingScore = 250; //cut off box front art here, chances that anything lower won't have art is high
 
         //all english regions with fixed dump [f]
         for (re in reRegion) {
-            if (item.match(reRegion[re]) && item.match(reOption.f) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion[re]) && item.match(reOption.f) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
         //all non-english regions with fixed dump [f]
         for (re in reRegion2) {
-            if (item.match(reRegion[re]) && item.match(reOption.f) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion[re]) && item.match(reOption.f) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
         //all english regions
         for (re in reRegion) {
-            if (item.match(reRegion[re]) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion[re]) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
         //all non-english regions
         for (re in reRegion2) {
-            if (item.match(reRegion2[re]) && resultrank < currentrank) {
-                result = item;
-                resultindex = i;
-                resultrank = currentrank;
+            if (item.match(reRegion2[re]) && itemScore < fallingScore) {
+                itemScore = fallingScore;
             }
-            --currentrank;
+            --fallingScore;
         }
 
         //no brackets
-        if (!item.match(reOption.b) && resultrank < currentrank) {
-            result = item;
-            resultindex = i;
-            resultrank = currentrank;
+        if (!item.match(reOption.b) && itemScore < fallingScore) {
+            itemScore = fallingScore;
         }
 
+        //compute! This the file's score better than the previous files?
+        if (itemScore > resultScore)
+        {
+            result = item;
+            resultindex = i;
+            resultScore = itemScore;
+        }
     }
 
     //if no matches, just take first item
@@ -245,7 +247,7 @@ FindBestRom.exec = function(files, officialscore) {
     return {
         game: result,
         index: resultindex,
-        rank: resultrank.toFixed(2)
+        rank: resultScore.toFixed(2)
     };
 };
 
